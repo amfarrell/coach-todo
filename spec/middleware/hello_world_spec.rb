b717edeb067c146e41d38264be2a3d3ac256828a
Andrew M. Farrell <amfarrell@mit.edu>
2017-05-16 19:57:28 +0100

    parameterize the person you say hello to.

--- spec/middleware/hello_world_spec.rb
+++ spec/middleware/hello_world_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe HelloWorld, type: :request do
   it 'returns 200 when visiting hello world' do
-    get '/hello_world'
+    get '/hello/world'
 
     expect(response.status).to eq(200)
+    expect(response.body).to include("hello world")
   end
 end
